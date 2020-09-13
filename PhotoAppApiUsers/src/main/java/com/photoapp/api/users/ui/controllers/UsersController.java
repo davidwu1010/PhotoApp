9b07a4f2a62b4cc61a4659a2076ec0c8f122ab7a
David Wu <wudawei01@gmail.com>
2020-09-13 14:34:04 +0100

    Add spring security

--- PhotoAppApiUsers/src/main/java/com/photoapp/api/users/ui/controllers/UsersController.java
+++ PhotoAppApiUsers/src/main/java/com/photoapp/api/users/ui/controllers/UsersController.java
@@ -9,6 +9,7 @@ import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +29,10 @@ public class UsersController {
         return "Working";
     }
 
-    @PostMapping
+    @PostMapping(
+        consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE},
+        produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE}
+        )
     public ResponseEntity<CreateUserResponseModel> createUser(@Valid @RequestBody CreateUserRequestModel userDetails) {
         ModelMapper modelMapper = new ModelMapper();
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
